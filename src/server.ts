dd97f397bbcb564740d4e0a9a51fdc4c7f8b9f36
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2023-08-16 18:32:32 -0300

    chore: added eslint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.ready()
-  .then(() => {
-    app.listen({
+app.ready().then(() => {
+  app
+    .listen({
       host: '0.0.0.0',
       port: env.PORT,
-    }).then(() => {
+    })
+    .then(() => {
       console.info(`🚀 HTTP Server Running on port ${env.PORT}`)
     })
-  })
+})
