9773f3fa25729d45636f132f68517fad6b46396d
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2023-08-24 23:20:21 -0300

    test: implements check-in routes e2e tests

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,40 @@
+import { it, describe, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const createGymResponse = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Lopes Gym',
+        description: 'Some description',
+        phone: '556134560080',
+        latitude: -15.8242778,
+        longitude: -48.0307621,
+      })
+
+    const { id } = createGymResponse.body.data
+
+    await request(app.server)
+      .post(`/gyms/${id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -15.8242778,
+        longitude: -48.0307621,
+      })
+      .expect(201)
+  })
+})
---
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -0,0 +1,49 @@
+import { it, describe, beforeAll, afterAll, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Validate check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to validate the check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Lopes Gym',
+        description: 'Some description',
+        phone: '556134560080',
+        latitude: -15.8242778,
+        longitude: -48.0307621,
+      },
+    })
+
+    const checkIn = await prisma.checkIn.create({
+      data: {
+        gym_id: gym.id,
+        user_id: user.id,
+      },
+    })
+
+    const response = await request(app.server)
+      .patch(`/check-ins/${checkIn.id}/validate`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+      .expect(200)
+
+    expect(response.body.data).toEqual(
+      expect.objectContaining({
+        validated_at: expect.any(String),
+      }),
+    )
+  })
+})
