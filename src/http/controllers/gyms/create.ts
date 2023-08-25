d97d9111199b23cdb7f4e49166a7fe79ba056106
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2023-08-24 22:15:15 -0300

    feat: implements create gym controller and routs refactor

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,38 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
+
+export async function createGym(request: FastifyRequest, reply: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string(),
+    latitude: z
+      .number()
+      .refine(
+        (val) => Math.abs(val) <= 90,
+        'Latitude must be between -90 and 90',
+      ),
+    longitude: z
+      .number()
+      .refine(
+        (val) => Math.abs(val) <= 180,
+        'Longitude must be between -180 and 180',
+      ),
+  })
+
+  const { description, title, phone, latitude, longitude } =
+    createGymBodySchema.parse(request.body)
+
+  const createGymUseCase = makeCreateGymUseCase()
+
+  const { gym } = await createGymUseCase.handle({
+    description,
+    latitude,
+    longitude,
+    phone,
+    title,
+  })
+
+  return reply.status(201).send({ data: gym })
+}
