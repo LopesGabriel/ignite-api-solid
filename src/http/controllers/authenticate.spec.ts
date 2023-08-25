d496f9f011fab0aef67d47b895589639fae89262
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2023-08-24 21:59:37 -0300

    test: implements authenticate e2e test

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,32 @@
+import { expect, it, describe, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate the user', async () => {
+    const email = 'john.doe@example.com'
+    const password = '123456'
+
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email,
+      password,
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email,
+      password,
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body.token).toEqual(expect.any(String))
+  })
+})
