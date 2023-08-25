6fc5aacb29cf2f54138d8aebeb2e2b3784f60e27
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2023-08-24 21:55:08 -0300

    test: implements register e2e test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,6 +1,29 @@
-import { expect, it } from 'vitest'
+import { expect, it, describe, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-it('ok', () => {
-  console.log('inside test')
-  expect(true).toBe(true)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(201)
+    expect(response.body.data).toEqual(
+      expect.objectContaining({
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+      }),
+    )
+  })
 })
